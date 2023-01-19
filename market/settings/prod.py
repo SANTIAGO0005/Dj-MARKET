7db622196b7bd1ce212bd293ad0f48cc3925ee3c
Santiago Cifuentes <cifuentess938@gmail.com>
2023-01-19 01:00:11 -0500

    activate debug

--- market/settings/prod.py
+++ market/settings/prod.py
@@ -1,7 +1,7 @@
 from .base import *
 import dj_database_url
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*','https://market-site-app.onrender.com']
 
