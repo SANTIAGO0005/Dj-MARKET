2b62aa7b2db4fe67dffe9e04c90515295b32717b
Santiago Cifuentes <cifuentess938@gmail.com>
2023-01-21 11:36:35 -0500

    resolved static errors

--- market/settings/prod.py
+++ market/settings/prod.py
@@ -13,7 +13,7 @@ DATABASES = {
 }
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR,'static']
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR,'media'
